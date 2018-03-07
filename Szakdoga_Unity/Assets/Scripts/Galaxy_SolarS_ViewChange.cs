d20d2cd1664600c95dda223762008a3aa002e705
Dani100015 <sandord302@gmail.com>
2018-03-07 13:27:16 +0100

    Galaxy&SolarSystem

---
+++ Szakdoga_Unity/Assets/Scripts/Galaxy_SolarS_ViewChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galaxy_SolarS_ViewChange : MonoBehaviour {
+
+	void Update () {
+		
+	}
+
+    public override void OnDeselect(BaseEventData data)
+    {
+        
+    }
+}
