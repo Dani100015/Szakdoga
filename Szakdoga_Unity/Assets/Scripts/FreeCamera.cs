e5b9dfa4fefdf59a17052edb274cb38e64eb0854
Dani100015 <sandord302@gmail.com>
2018-03-28 13:02:36 +0200

    SceneManagment

---
+++ Szakdoga_Unity/Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour {
+
+    [SerializeField] private float speed;
+
+	void Update ()
+    {
+
+		if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime , 0f,0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0f, 0f, speed * Time.deltaTime); ;         
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= new Vector3(0f, 0f, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Szakdoga_Unity/Assets/Scripts/Galaxy_SceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Galaxy_SceneManager : MonoBehaviour {
+
+
+    GameObject selectedObject;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                if (selectedObject != hitInfo.transform.gameObject)
+                {
+                    selectedObject = hitInfo.transform.gameObject;
+                }
+                else
+                {
+                    SceneManager.LoadScene(selectedObject.name.ToString());
+                }
+            }
+        }
+    }
+}
