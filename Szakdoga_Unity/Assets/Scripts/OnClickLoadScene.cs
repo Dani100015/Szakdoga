6abf820a7e471fab670833b3065a6cec45dd8fad
Dani100015 <sandord302@gmail.com>
2018-09-16 22:54:19 +0200

    MainMenu

---
+++ Szakdoga_Unity/Assets/Scripts/OnClickLoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class OnClickLoadScene : MonoBehaviour {
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+	
+}
---
+++ Szakdoga_Unity/Assets/Scripts/SetSelectedButtonOnRawAxis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetSelectedButtonOnRawAxis : MonoBehaviour {
+
+    public GameObject selectedButtonGameObejct;
+    public EventSystem eventSystem;
+
+    private bool buttonSelected;
+
+	void Update () {
+
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedButtonGameObejct);
+            buttonSelected = true;
+        }
+    }
+
+    void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
