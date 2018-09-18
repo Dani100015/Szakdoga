14476a4f9349515f93c9fe7c9a31520e64a766e9
Dani100015 <sandord302@gmail.com>
2018-09-18 13:04:49 +0200

    Basic Classes

---
+++ Szakdoga_Unity/Assets/Scripts/Basic Classes/Celestials.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celestials {
+
+    int x;
+    int y;
+
+    string name;
+    SolarSystem solarSystem;
+
+
+}
