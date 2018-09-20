23733780d300d9ac7e9c088ba374ccb81866919b
tothp0096 <tothp0093@gmail.com>
2018-09-20 13:04:16 +0200

    Alap osztályok

---
+++ Szakdoga_Unity/Assets/Scripts/Basic Classes/Structure.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : Unit
+{
+    
+    List<Tech> ResearchableTechs;
+    Vector3 RallyPoint;
+    Queue TrainingQueue;
+    int TimeLeft;
+    bool Training;
+
+    void Start()
+    {
+        isWalkable = false;
+        ResearchableTechs = new List<Tech>();
+        TrainingQueue = new Queue();
+    }
+
+    void StartTrain(Unit unit)
+    {
+        Training = true;
+        TimeLeft = unit.trainingTime;
+    }
+
+    void StartResearch(Tech tech)
+    {
+        Training = true;
+        TimeLeft = tech.ResearchTime;
+    }
+
+    void EndTrain()
+    {
+       
+    }
+
+    void EndResearch()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Training)
+        {
+            if (TimeLeft != 0)
+                TimeLeft -= Mathf.RoundToInt(Time.deltaTime);
+            else
+            {
+                Training = false;
+                if (TrainingQueue.Peek() is Unit)
+                { }
+
+            }
+        }
+    }
+}
--- Szakdoga_Unity/Assets/Scripts/Basic Classes/Tech.cs
+++ Szakdoga_Unity/Assets/Scripts/Basic Classes/Tech.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tech : MonoBehaviour {
+public class Tech
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //alap adatok
+    string name;
+    string description;
+    Tech prerequisite;
+    Tech leadsTo;
+    int researchTime;
+    bool researched;
+
+    //nyersanyag szükséglet
+    int palladiumCost;
+    int iridiumCost;
+    int eezoCost;
+
+    #region Propertyk 
+    public bool Researched
+    {
+        get; set;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public Tech Prerequisite
+    {
+        get { return prerequisite; }
+        set { prerequisite = value; }
+    }
+
+    public Tech LeadsTo
+    {
+        get { return leadsTo; }
+        set { leadsTo = value; }
+    }
+
+    public int ResearchTime
+    {
+        get { return researchTime; }
+        set { researchTime = value; }
+    }
+
+    public int PalladiumCost
+    {
+        get { return palladiumCost; }
+        set { palladiumCost = value; }
+    }
+
+    public int IridiumCost
+    {
+        get { return iridiumCost; }
+        set { iridiumCost = value; }
+    }
+
+    public int EezoCost
+    {
+        get { return eezoCost; }
+        set { eezoCost = value; }
+    }
+    #endregion
+
+    public override string ToString()
+    {
+        return this.Name;
+    }
 }
